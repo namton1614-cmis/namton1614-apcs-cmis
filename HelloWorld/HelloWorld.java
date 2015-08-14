23f0310dcf1f8ce665a5c0cb7499bd282d29652a
cmis <cmis@cmis-hs16.(none)>
2015-08-14 13:49:31 +0700

    Hello World Program

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld 
+{
+  public static void main ( String args[] ) // method signature: Consists of modifiers (public, static, void) and a method name (main). 
+  {
+    System.out.println("Hello, World!");
+  } // end main
+} // end HelloWorld
