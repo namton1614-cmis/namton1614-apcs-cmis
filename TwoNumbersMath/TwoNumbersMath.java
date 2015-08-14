96c2bc2b41735478fdadceb14e5a26648bc3988a
cmis <cmis@cmis-hs16.(none)>
2015-08-14 14:35:38 +0700

    How to Math

--- TwoNumbersMath/TwoNumbersMath.java
+++ TwoNumbersMath/TwoNumbersMath.java
@@ -1,13 +1,19 @@
 import javax.swing.JOptionPane;  
+
 class TwoNumbersMath{  
-  public static void main ( String args[] ) 
+  public static void main ( String args [] ) 
  {  
    int sum = 0;
    
-   String imput = JOptionPane.showImputDialog( "s" + "s");
+   String input = JOptionPane.showInputDialog( "s" + "s");
    
    System.out.println ( input );
-     
+   
+   int num = Integer.parseInt ( input );
+   
+   sum = 5 + num;
+   
+   System.out.println ( sum );
 
  }  // end main
 }   // end class InputData
