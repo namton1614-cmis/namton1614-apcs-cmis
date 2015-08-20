a45f5d7b9dfa96cb9bb9ca65076b5e4498bfb991
cmis <cmis@cmis-hs16.(none)>
2015-08-20 14:37:18 +0700

    FundP4

---
+++ FundamentalsP4/FundamentalsP4.java
@@ -0,0 +1,11 @@
+
+public class FundamentalsP4
+{
+  public static void main ( String args[] ) 
+  {
+   for( int base = 1; base < 11;base++ )
+        {
+        System.out.println ( base + "\t" + base*base + "\t" + ( int )(base+base*base*Math.random () ));
+        } // end for
+  } // end main
+} // end class FundamentalsP4
