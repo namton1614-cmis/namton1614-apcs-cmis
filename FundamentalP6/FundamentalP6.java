24b0b1d72351dda01d52bab946843444e62a845e
cmis <cmis@cmis-hs16.(none)>
2015-08-26 14:35:14 +0700

    Fundamentals6

---
+++ FundamentalP6/FundamentalP6.java
@@ -0,0 +1,25 @@
+
+public class FundamentalP6
+{
+    public static void main (String argvs [] )
+    {
+      int[] maxArray = new int[50];
+    
+      int max = maxArray.length;
+      for ( int index = 0; index < max; index++ )
+      {
+        
+          maxArray[ index ] = (int)(Math.random()*100   );     
+        
+        } //end for
+      for ( int index = 0; index < max; index++ )
+      {
+        
+          System.out.print( maxArray[ index ] + " " );    
+      
+        } //end for
+      System.out.print( "\n" );    
+      
+    }//end main
+}// end class FundamentalsP6
+  
