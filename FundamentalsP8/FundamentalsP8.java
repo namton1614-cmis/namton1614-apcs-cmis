2d357f41e45672e4e77c30e99ab977d2eba4830d
cmis <cmis@cmis-hs16.(none)>
2015-08-31 15:01:59 +0700

    Fundamentals 8 done

--- FundamentalsP8/FundamentalsP8.java
+++ FundamentalsP8/FundamentalsP8.java
@@ -2,35 +2,35 @@ public class FundamentalsP8
 {
     public  static void main (String argvs [] )
     {
-     String [] Singers = new String[3]   ; 
-     int k = 0;
-     int l = 1;
-     int g = 2;
-     Singers[k] = "A";
-     Singers[l] = "B";
-     Singers[g] = "C";  
+     String [] singers = new String[3]   ; 
+     singers[0] = "Taylor Swift";
+     singers[1] = "Serj Tankian";
+     singers[2] = "Kanye West";  
      
-     for ( int index = 0; index <Singers.length; index++)
+     for ( int index = 0; index <singers.length; index++)
      {
-       System.out.println( Singers [index] );
+       System.out.println( singers [index] );
         }
+     System.out.println("\n");
      int x = (int)(Math.random()*(3));
-        Singers[x] = "Michael Jackson";
-    for ( int index = 0; index <Singers.length; index++)
+     singers[x] = "Michael Jackson";
+    for ( int index = 0; index <singers.length; index++)
      {
-       System.out.println( Singers [index] );
+       System.out.println( singers [index] );
         }
-     for ( int index = 0; index <Singers.length; index++)
+     System.out.println("\n");
+     for ( int index = 0; index <singers.length; index++)
      {
 
-       if (k==x)
+       if ( singers[index].equals("Michael Jackson"))
        {
                System.out.println( "We have found Michael Jackson." );
         }
         else
        {
-            System.out.println( Singers[index]+ " is not Michael Jackson.");
+            System.out.println( singers[index]+ " is not Michael Jackson.");
         }
     }
+     System.out.println("\n");
     }//end main
 }//end Fundamentals P8
