872cebcd21fc6b0ea06e540db6eb6edfc389b357
cmis <cmis@cmis-hs16.(none)>
2015-09-03 09:55:44 +0700

    Fixses

--- FundamentalsP7/FundamentalsP7.java
+++ FundamentalsP7/FundamentalsP7.java
@@ -4,19 +4,16 @@ public class FundamentalsP7
     public static void main (String argvs [] )
     {
       int[] intHold = new int[11];
-      int x = 0;
+      int positiveCount = 0;
       for ( int index = 0; index < intHold.length; index++ )
       {
-        int a = (int) (Math.random()*100);
-        int k = a % 2;
-
-         if( k ==1 )
+         if( Math.random() <= 0.5 )
           {
            intHold[ index ] = (int)((Math.random()*100)*(-1) ); 
          }
          else 
          {
-           x++;
+           positiveCount++;
            intHold[ index ] = (int)((Math.random()*100)*1  );     
          }              
         } //end for
@@ -27,7 +24,7 @@ public class FundamentalsP7
           System.out.print( intHold[ index ] + " \n" );    
       
         } //end for
-      System.out.println( "Number of positive integers = " + x + "\n" );    
+      System.out.println( "Number of positive integers = " + positiveCount + "\n" );    
       
     }//end main
 }// end class FundamentalsP7
--- FundamentalsP9/FundamentalsP9.java
+++ FundamentalsP9/FundamentalsP9.java
@@ -3,26 +3,20 @@ public class FundamentalsP9
 {
     public static void main (String argvs [])
     {
-        int x = 0;
-        while ( x != 314159)
+      Scanner input = new Scanner(System.in);
+      int password = 0;
+        while ( password !=314159)
         {
         System.out.println("\nInput Username.");
-        Scanner input = new Scanner(System.in);
+
         String username = input.nextLine();
 
         if ( username.equals("SecondRight" ))
         {
             System.out.println("\nInput Password.");
-             Scanner inputpass = new Scanner(System.in);
-             String password = inputpass.nextLine();
-            int passNumber = Integer.parseInt ( password ); 
-          if (password.equals("314159"))
+             password = input.nextInt();
+          if (password ==314159 )
           {
-
-              while (x<314159)
-              {
-               x++ ;
-            }
             System.out.println( "Welcome, SecondRight." ); 
            }
            else 
@@ -36,5 +30,5 @@ public class FundamentalsP9
         }
 
       }
-    }
+    }// end main
 }// end class FundamentalsP9
