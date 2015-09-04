9275719c1f522375e477e12fc9ae6e7df1d9af96
cmis <cmis@cmis-hs16.(none)>
2015-09-04 11:17:25 +0700

    TestOne

---
+++ TestOne/TestOne.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;  
+public class TestOne
+{
+    public static void main (String argvs [] )
+    {
+      String ask = JOptionPane.showInputDialog( "Give me a number!" );
+      int s = Integer.parseInt ( ask );
+        
+      int[] intHold = new int[s];        
+      for ( int index = 0; index < intHold.length; index++ )
+      {
+         
+           intHold[ index ] = (int)((Math.random()*50)); 
+             float intHold[] = Float.parseFloat ( first );
+           System.out.print( intHold[ index ] + " \n" );    
+        
+          
+                   
+        } //end for        
+        
+        
+        
+
+       
+      
+    }//end main
+}// end class TestOne
