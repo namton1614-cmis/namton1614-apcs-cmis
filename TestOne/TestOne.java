77d7d972f32b874acb09060e07d70fcd79b1d666
cmis <cmis@cmis-hs16.(none)>
2015-09-04 11:45:48 +0700

    TestOneSoon

--- TestOne/TestOne.java
+++ TestOne/TestOne.java
@@ -10,14 +10,48 @@ public class TestOne
       for ( int index = 0; index < intHold.length; index++ )
       {
          
-           intHold[ index ] = (int)((Math.random()*50)); 
-             float intHold[] = Float.parseFloat ( first );
-           System.out.print( intHold[ index ] + " \n" );    
-        
+           intHold[ index ] = (int)(Math.random()*50); 
+
+           System.out.print( index + ":" + intHold[ index ] + " \n"  );    
+
           
                    
         } //end for        
+       int index = intHold.length-1;
+        while ( index >= 0   )
+      {
+          
+         
+
+    int k =intHold[ index ] % 2;
+
+   if( k ==1 )
+
+        {      System.out.print( index + ":");
+           int as = 0;
+            while ( as < intHold[ index ] )
+            {
+           System.out.print( "O")    ;
+           index++;
+        }
+           System.out.print( intHold[ index ] + " \n" );            
+        }
+   else 
+        {
+             System.out.print( index + ":");
+            int as = 0;
+            while ( as < intHold[ index ] )
+            {
+           System.out.print( "E")    ;
+           index++;
+        }
+           System.out.print( intHold[ index ] + " \n" );      
+        }           
         
+        
+          index--;
+                   
+        } //end for               
         
         
 
