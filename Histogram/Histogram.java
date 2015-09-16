5f59ebae4fc0594bd8d5607408c62f2a1813e7b4
cmis <cmis@cmis-hs16.(none)>
2015-09-16 09:47:19 +0700

    Histogram donezo

--- Histogram/Histogram.java
+++ Histogram/Histogram.java
@@ -7,7 +7,10 @@ public class Histogram
         int length = Integer.parseInt ( input );
         int [] randomNumbers = new int [length];        
         int [] count = new int [100];
-
+        int sum = 0;
+        int max = 0;
+        int min = 99;
+        int mode = 0;
         for ( int index = 0; index < randomNumbers.length; index++ )
         {
 
@@ -23,13 +26,41 @@ public class Histogram
         for ( int index = 0; index <= 99; index++ )
         {
             System.out.print( index + ":"  );    
-            for (int newindex = 0; newindex <= count[index]; newindex++)
+            for (int newindex = 0; newindex < count[index]; newindex++)
             {
                 System.out.print( "#" );    
 
             }
             System.out.println( ""  );    
-        }
+        }        
         // end histogram generation
+        for (int i : randomNumbers)
+        { 
+            sum += i ;
+        }
+        for ( int index = 0; index < length; index++ )
+        {
+            if (randomNumbers [index] >= max)
+            {
+                max = randomNumbers [ index];
+            }
+            if (randomNumbers [index] <= min)
+            {
+                min = randomNumbers [ index];
+            }
+        }
+        for ( int index = 0; index <= 99; index++ )
+        {
+            if (count [index] >= mode)
+            {
+                mode = count[index];
+            }
+        }
+
+        System.out.println( "MIN:" + min  );           
+        System.out.println( "MAX:" + max  );
+        System.out.println( "SUM:" + sum  );   
+        System.out.println( "AVG:" + sum/length  );    
+        System.out.println( "MST:" + mode );    
     } // end main
-    }
+}
