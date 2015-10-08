8667425bbd7dc73974118da50746cf66a733aed4
cmis <cmis@cmis-hs16.(none)>
2015-10-08 14:07:43 +0700

    Fixed CD1-2

--- ClassDesignP1/SongDriver.java
+++ ClassDesignP1/SongDriver.java
@@ -1,12 +1,10 @@
 
 public class SongDriver {
-
     public static void main (String argvs [] )
     {
-        Song otherSong = new Song ();
-        Song thisSong = new Song("Zyon", 34, "Jizks", true);
-
-        System.out.println ( thisSong);
-        System.out.println (otherSong);
+        Song song1 = new Song ();
+        System.out.println ( "Song 1  \n" +song1);
+        Song song2 = new Song("Auferstehungs-Symphonie", "Mahler", 4.04);
+        System.out.println ( "Song 2  \n" +song2);
     }
 } //end class MyPhoneDriver
