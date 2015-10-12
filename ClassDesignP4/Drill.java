d9ea7d94acdf6c581bbd5c432bee927bdb82ab12
cmis <cmis@cmis-hs16.(none)>
2015-10-12 11:51:40 +0700

    tuna fish

---
+++ ClassDesignP4/Drill.java
@@ -0,0 +1,30 @@
+
+public class Drill
+{
+    private int gradelevel;
+    private String firstname;
+    private String lastname;
+    private double gpa;
+    public Drill()
+    {
+        gradelevel =12;
+        firstname = new String ("Mina");
+        lastname = new String ("Hakuba");
+        gpa = 3.75;
+    } //end constructor Song
+    public Drill (String firstname, String lastname, int gradelevel, double gpa)
+    {
+        this.firstname = new String (firstname);
+        this.lastname = new String (lastname);
+        this.gradelevel = gradelevel;
+        this.gpa = gpa;
+    }
+
+    public String toString( )
+    {
+        String output = new String();
+        output = "'s full name is " + firstname +" " +lastname + " and has a gpa of "  + 
+        gpa + " studying at grade "+ gradelevel +"." ;
+        return output;
+    }
+}
---
+++ ClassDesignP4/DrillDriver.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+public class DrillDriver {
+    public static void main (String argvs [] )
+    {
+        Drill senior001 = new Student ();
+        System.out.println ( "Senior001" +senior001);
+        Drill junior001 = new Student ("Graham", "Jones", 11, 4.00);
+        System.out.println ( "Junior001" +junior001);
+        JOptionPane.showMessageDialog(null, "Senior001"+ senior001 +"  Junior001" +junior001, "Junior001 and Senior001", JOptionPane.INFORMATION_MESSAGE ) ;
+    }
+} //end class StudentDriver
