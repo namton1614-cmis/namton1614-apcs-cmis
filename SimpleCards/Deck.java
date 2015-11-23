e897a20cb7e7c171b392fff15921eeecba6abdcc
cmis <cmis@cmis-hs18.(none)>
2015-11-23 14:18:32 +0700

    Attempt to make a deck class

---
+++ SimpleCards/Deck.java
@@ -0,0 +1,19 @@
+
+public class Deck
+{
+    private Card [] cardsList;
+    private int deckNumber;
+
+    public Deck()
+    {
+        this.cardsList []= new Card [] cardslist;
+        this.deckNumber = deckNumber;
+    }
+
+
+
+    public String toString()
+    {
+        return String.format(rank +suit);
+    }
+}
