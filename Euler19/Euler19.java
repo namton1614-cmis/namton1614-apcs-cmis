f7509e2600a2dd44e304392d0d24d1604840f377
cmis <cmis@cmis-hs16.(none)>
2015-12-02 11:16:24 +0700

    EUler 19 wrong

--- Euler19/Euler19.java
+++ Euler19/Euler19.java
@@ -5,36 +5,50 @@ public class Euler19
     {
         int year=1901;
         int tDay =0;
+        int sundays =0;
         while (year!=2000)
         {
             int month =1;
             while(month<=12)
             {
                 int day =1;
-                
-                 if (month == 2)
+                int monthL=31;
+                if (month == 2)
                 {
-                    int monthL=28;
-                    
+                    int k= year%4;
+                    if (k==0)
+                    {
+                        monthL=29;
+                    }
+                    else
+                    {
+                        monthL=28;
+                    }
+
                 } 
                 else if (month == 4||month ==9 || month ==11|month == 6)
                 {
-                    int monthL=30;
+                    monthL=30;
                 } 
-                else 
-                {
-                   int monthL=31;
-                
-                }
 
-                if(day!=31)
+                while(day<monthL)
                 {
-                    day++;
                     tDay++;
+                    int monday = tDay&7;
+                    if (monday==0 && day==1)
+                    {
+                        sundays++;
+
+                    }
+                    day++;
+
+           
                 }
+                System.out.println(year + "\t"+ month +"\t" + day + "\t"+sundays);
                 month++;
             }
             year++;
+
         }
         System.out.println(tDay);
     }
