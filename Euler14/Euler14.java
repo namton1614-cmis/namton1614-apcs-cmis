d243d679c768cf35d81ebfd82015525f30b64231
cmis <cmis@cmis-hs16.(none)>
2015-12-09 11:27:55 +0700

    Euler  14 fixed and donezo

--- Euler14/Euler14.java
+++ Euler14/Euler14.java
@@ -4,10 +4,11 @@ public class Euler14
     public static void main (String arzs [])
     {
         long k = 0;
-        int f =0;
-        for (int i =999990;i<=1000000;i++)
+        long f =0;
+        long a=0;
+        for (long i =2;i<=1000000;i++)
         {
-            int m =0;
+            int m =1;
             k =i;
             while (k!=1)
             {
@@ -21,16 +22,17 @@ public class Euler14
                     k =3*k+1;
                 }
                 m++;
-                System.out.println(k + "\t"+ m);
+
 
             }
-            if (m>f)
+            if (m>=f)
             {
                 f=m;
+                a=i;
             }
         }
 
-        System.out.println(f);
+        System.out.println(f +"\t"+a);
     }
 
 }
