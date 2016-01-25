5ee9ed05c8954c806d08033e5a453ff1b3b6528c
cmis <cmis@cmis-hs14.(none)>
2016-01-25 14:34:37 +0700

    TRAILS IN THE SKY

--- ClassCompositionBigProblem/Trail.java
+++ ClassCompositionBigProblem/Trail.java
@@ -33,16 +33,65 @@ public class Trail     {
         return sum;
     }
 
+    public boolean isLevelTrailSegment( int begin, int end )
+    {
+        boolean check = true;
+        if (markers.get(begin)==markers.get(end))
+        {
+            for (int i=0;i<markers.size();i++)
+            {
+                if (i>0&&Math.abs(markers.get(i)-markers.get(i-1))>10)
+                {
+                    check=false;
+
+                }
+            }
+        }
+        if (check)
+        {
+            return true;
+        }
+        else 
+            return false;
+    }
+
+    public boolean isDifficult(int begin, int end)
+    {
+        int netElevation=0;
+        if (!isLevelTrailSegment(begin, end))
+        {
+            for (int i=0;i<markers.size();i++)
+            {
+                if (i>0|| markers.get(i)-markers.get(i-1)<0)
+                {
+                    int subtraction=Math.abs(markers.get(i)-markers.get(i+1));
+                    netElevation+=subtraction;
+
+                }
+            }
+
+        } 
+        if (netElevation>100)
+        {
+            return true;
+        }
+        else 
+
+            return false;
+
+    }
+
     public String toString( )
     {
-        String ok ="Index\tElevation\n" ;
+        String output ="Index\tElevation\n" ;
         int i=0;
         for (Integer x: markers)
         {   i++;
-            ok += i+"\t"+x+"\n";
+            output += i+"\t"+x+"\n";
         }
-        String output = new String();
-        output =  ok  ;
+        output += "Total Distance of Trail is :"+getLength()+"\n";
+        output += "Trail Level: " +  isLevelTrailSegment( 0,markers.size()-1);
+        output += "Trail Difficult: " +         isDifficult(0,markers.size()-1);
         return output;
     }
 }
--- ClassCompositionBigProblem/TrailDriver.java
+++ ClassCompositionBigProblem/TrailDriver.java
@@ -1,9 +1,27 @@
- 
+import java.util.ArrayList;
 public class TrailDriver     {
     public static void main (String argvs [] )
     {
-        Trail rng = new Trail ();
-        System.out.println ( rng);
-         
+        ArrayList<Integer> neon = new ArrayList<Integer>(); 
+        neon.add( 60 );
+        neon.add( 55 );
+        neon.add( 60 );
+        neon.add( 55 );                    
+        neon.add( 60 );
+        neon.add( 60 );
+        neon.add( 60 );
+        neon.add( 60 );   
+
+        Trail Fuji = new Trail ();
+        Trail Everest = new Trail ();
+        Trail Cook = new Trail (neon);
+
+        Cook.addMarker(60);
+        Cook.addMarker(50);
+        Cook.addMarker(60);
+        Cook.addMarker(60);
+        Cook.addMarker(60);
+        System.out.println ("\nFuji\n"+ Fuji +"\nCook\n"+ Cook +"\nEverest\n"+ Everest);
+
     }
 } //end class  
