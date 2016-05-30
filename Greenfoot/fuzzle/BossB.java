ae22033766cebbb5443b6d36bf4c944d6c796184
cmis <cmis@cmis-hs16.(none)>
2016-05-30 11:43:41 +0700

    Loopers qq qq qq

--- Greenfoot/fuzzle/BossB.java
+++ Greenfoot/fuzzle/BossB.java
@@ -10,6 +10,8 @@ public class BossB extends Boss implements ChangeBoss
 {
     ArrayList<Bullets> fire = new ArrayList<Bullets>(); 
     int counter2=0;
+    int i=0;
+    int k=0;
     /**
      * Act - do whatever the BossA wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -17,39 +19,28 @@ public class BossB extends Boss implements ChangeBoss
     public  BossB() 
     {
         super(180,50);
-        fire.add(new Type1(0));
-        fire.add(new Type1(45));
-        fire.add(new Type1(90));
-        fire.add(new Type1(135));
-        fire.add(new Type1(180));
-        fire.add(new Type1(225));
-        fire.add(new Type1(270));
-        fire.add(new Type1(315));
-        fire.add(new Type1(360));
-        fire.add(new Type2(0));
-        fire.add(new Type2(45));
-        fire.add(new Type2(90));
-        fire.add(new Type2(135));
-        fire.add(new Type2(180));
-        fire.add(new Type2(225));
-        fire.add(new Type2(270));
-        fire.add(new Type2(315));
-        fire.add(new Type2(360));
+    while( i<=360)
+            {
+                fire.add(new Type1(i));
+                i+=45;
+            }
+        while( k<=360)
+            {
+                fire.add(new Type2(k));
+                k+=45;
+            }
     }    
 
     public void spray()
     {
         World world = getWorld();
-        counter2++;
+
         for (Bullets x:fire )
         {
             world.addObject(x,this.getX(),this.getY());
         }
-        if (counter2>10)
-        { counter2=0;
-
-        }
-    }
+     }
+    
 
     public void nextboss()
     {
--- Greenfoot/fuzzle/BossC.java
+++ Greenfoot/fuzzle/BossC.java
@@ -10,6 +10,7 @@ public class BossC extends Boss implements ChangeBoss
 {
     ArrayList<Bullets> fire = new ArrayList<Bullets>(); 
     int counter2=0;
+    int i=0;
     /**
      * Act - do whatever the BossA wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -19,6 +20,7 @@ public class BossC extends Boss implements ChangeBoss
         super(220,50);
 
     }    
+
     public void spray()
     {
         World world = getWorld();
@@ -27,27 +29,19 @@ public class BossC extends Boss implements ChangeBoss
         {
             world.addObject(x,this.getX(),this.getY());
         }
-        if (counter2>10)
+        if  (counter2>10)
         { counter2=0;
-            fire.add(new Type1(0));
-            fire.add(new Type1(45));
-            fire.add(new Type1(90));
-            fire.add(new Type1(135));
-            fire.add(new Type1(180));
-            fire.add(new Type1(225));
-            fire.add(new Type1(270));
-            fire.add(new Type1(315));
-            fire.add(new Type1(360));
+            i=0;
+            while( i<=360)
+            {
+                fire.add(new Type1(i));
+                i+=45;
+            }
         }
     }
-    
-    
+
     public void nextboss()
     {
-        World world = getWorld();
-
-       BossD bossD = new BossD();
-        world.addObject(bossD,this.getX(),this.getY());
-        world.removeObject(this);
+        Greenfoot.setWorld(new Win());
     }
 }
